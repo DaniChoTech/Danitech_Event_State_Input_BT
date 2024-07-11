758d12c7dcbab8ac9521c416800c4fda73234ca6
조현승 <danitech171791@gmail.com>
2024-07-11 13:06:18 +0900

    [Update] BT - 노드 / BT에서 사용할 노드 추가 / 노드의 상태 enum 추가
    - BT에서 사용할 노드는 IBTNode를 상속받아 구현하게 된다
    - 노드를 구현할 때 상속을 해줄 것이므로, 인터페이스형이다.
    - EBTNodeState라는 노드의 현재 상태를 표현할 수 있는 열거형(enum)을 추가했다.
    - 노드와 관련된 상태는 성공,실패,진행중이 있다.

---
+++ Assets/Scripts/BT/IBTNode.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBTNode
+{
+    public enum EBTNodeState
+    {
+        Success,
+        Fail,
+        Running
+    }
+
+}
