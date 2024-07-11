68b3e356d4e1d3b2f94340274aaf54e4dcbe3956
조현승 <danitech171791@gmail.com>
2024-07-11 13:29:59 +0900

    [init] BT - 액션노드 추가
    - 액션 노드란 실제로 어떤 행동을 하는 역할을 가진 노드이다.
    - 주로 셀렉터나, 시퀀스 노드 하위에 소속되기도 한다. (물론 직접 액션노드를 루트에 넣어줄 수도 있음)
    - 주로 (그림)과 같이 트리의 말단에 위치하는 경우가 많다.

---
+++ Assets/Scripts/BT/PetActionNode.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 행동대장 - 실제로 어떤 행동을 하는 노드
+public class PetActionNode : IBTNode
+{
+    Func<IBTNode.EBTNodeState> _onUpdate = null;
+}
