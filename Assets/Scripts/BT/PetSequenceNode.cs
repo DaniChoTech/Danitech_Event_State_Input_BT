94c5c2f69afae9cf52d15e2bf006fe7142dc50f3
조현승 <danitech171791@gmail.com>
2024-07-11 15:02:59 +0900

    [Update] BT - 노드 / 시퀀스 노드 Evaluate
    - 쭉 진행하다가 [진행중]이 반환되면 해당 작업을 중단한다. 그리고 나중에 이 시퀀스노드에 들어왔을 때 유지된 currentChild부터 재실행한다.
    - Running과 Fail은 해당 시퀀스노드를 종료시킨다.

--- Assets/Scripts/BT/PetSequenceNode.cs
+++ Assets/Scripts/BT/PetSequenceNode.cs
@@ -6,9 +6,39 @@ using UnityEngine;
 public class PetSequenceNode : IBTNode
 {
     List<IBTNode> _childNodeList;
+    int _currentChild;
 
     public PetSequenceNode(List<IBTNode> childNodeList)
     {
         _childNodeList = childNodeList;
+        _currentChild = 0;
     }
+
+    // 자식 -> Running -> Running 반환
+    // 자식 -> Success -> 다음 자식 이동
+    // 자식 -> Fail -> Fail 반환
+    // 시퀀스 노드는 Running이면, 그 상태를 계속 유지해야함 (ex. 플레이어를 따라갈 때까지 이동 유지)
+    // 자식으로 이동하면 안되고, 다음 프레임도 그 자식 평가를 진행해야함
+    public IBTNode.EBTNodeState Evaluate()
+    {
+        if (_childNodeList == null || _childNodeList.Count == 0)
+            return IBTNode.EBTNodeState.Fail;
+
+       for(; _currentChild < _childNodeList.Count; _currentChild++)
+        {
+            var childState = _childNodeList[_currentChild].Evaluate();
+            switch (childState)
+            {
+                case IBTNode.EBTNodeState.Running:
+                    return IBTNode.EBTNodeState.Running;
+                case IBTNode.EBTNodeState.Fail:
+                    _currentChild = 0;
+                    return IBTNode.EBTNodeState.Fail;
+            }
+        }
+
+        _currentChild = 0;
+        return IBTNode.EBTNodeState.Success;
+    }
+
 }
